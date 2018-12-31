3c7a9b6e07ede27e3c94eeb27a242ec022d2e6d2
quezan <quezanw@gmail.com>
2018-12-31 15:00:16 -0800

    nested comment details in card component

--- src/index.js
+++ src/index.js
@@ -1,27 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
 	return (
 		<div className="ui container comments">
-			<div className="comment">
-				<a href="/" className="avatar">
-					<img alt="avatar" />
-				</a>
-				<div className="content">
-					<a href="/" className="author">
-						Sam
-					</a>
-					<div className="metadata">
-						<span className="date">
-							Today at 6:00pm
-						</span>
-					</div>
-					<div className="text">
-						Nice blog post!
-					</div>
-				</div>
-			</div>
+			<ApprovalCard>
+				<CommentDetail 
+					author="Sam" 
+					time="3:30pm" 
+					comment="Nice blog post!" 
+					avatar={faker.image.avatar()}
+				/>
+			</ApprovalCard>
+			<ApprovalCard>
+				<CommentDetail 
+						author="Alex" 
+						time="4:12am" 
+						comment="anyone awake?"
+						avatar={faker.image.avatar()}
+					/>
+			</ApprovalCard>
+			<ApprovalCard>
+				<CommentDetail 
+					author="Jane" 
+					time="9:03am" 
+					comment="Wow react is cool!" 
+					avatar={faker.image.avatar()}
+				/>
+			</ApprovalCard>
 		</div>
 	);
 };
